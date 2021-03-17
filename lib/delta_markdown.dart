c97a822b027de6f7fc190aa17aaea8b85f3a37b1
Till Friebe <friebetill@gmail.com>
2021-03-17 10:52:14 +0100

    Add two helper methods to simplify usage

--- lib/delta_markdown.dart
+++ lib/delta_markdown.dart
@@ -7,6 +7,17 @@ import 'package:flutter_quill/models/quill_delta.dart';
 import 'src/delta_markdown_decoder.dart';
 import 'src/delta_markdown_encoder.dart';
 
+/// Codec used to convert between Markdown plain text and Quill deltas.
+const DeltaMarkdownCodec _kCodec = DeltaMarkdownCodec();
+
+Delta markdownToDelta(String markdown) {
+  return _kCodec.decode(markdown);
+}
+
+String deltaToMarkdown(Delta delta) {
+  return _kCodec.encode(delta);
+}
+
 class DeltaMarkdownCodec extends Codec<Delta, String> {
   const DeltaMarkdownCodec();
 
