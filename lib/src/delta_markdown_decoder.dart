85c8a3853067c49238b003b6bb3f4b2b5c06c87f
devtud <devtud@gmail.com>
2022-02-03 22:06:00 +0100

    Adapt imports for flutter_quill>3.4.4

--- lib/src/delta_markdown_decoder.dart
+++ lib/src/delta_markdown_decoder.dart
@@ -1,8 +1,8 @@
 import 'dart:collection';
 import 'dart:convert';
 
-import 'package:flutter_quill/models/documents/attribute.dart';
-import 'package:flutter_quill/models/quill_delta.dart';
+import 'package:flutter_quill/flutter_quill.dart'
+    show Attribute, AttributeScope, Delta, LinkAttribute;
 
 import 'ast.dart' as ast;
 import 'document.dart';
